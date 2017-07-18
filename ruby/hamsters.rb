7f72d2937e51be300cd5ebff132ddda4f1a489ef
Maria Luisa Rgit config --global user.name Eleanor <maluisa.rc@gmail.com>
2017-07-18 12:41:34 -0700

    Create template for hamster registration

---
+++ ruby/hamsters.rb
@@ -0,0 +1,31 @@
+puts "Hamster's name:"
+hamster_name = gets.chomp
+puts "Volume level:"
+volume_level = gets.chomp.to_i
+puts "Fur color:"
+fur_color = gets.chomp
+puts "Good candidate for adoption (Y/N)?"
+candidate = gets.chomp
+if candidate == "y"
+  candidate = true
+else candidate = false
+end
+puts "Estimated age:"
+age = gets.chomp.to_i
+if age == ""
+  age = nil
+end
+
+
+
+puts "Hamster's name is: #{hamster_name}"
+puts "Volume level is: #{volume_level}"
+puts "Fur color is: #{fur_color}"
+puts "Estimated age: #{age}"
+if candidate == true
+  puts "#{hamster_name} is a good hamster for adoption"
+else
+  puts "#{hamster_name} is NOT a good hamster for adoption"
+end
+
+
