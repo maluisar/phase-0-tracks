e4d7fa02e5a15f1e93883eb6861e23c11ab4de39
Maria Luisa Rgit config --global user.name Eleanor <maluisa.rc@gmail.com>
2017-08-02 11:34:34 -0700

    Create two classes that include module Shout and add Driver Code

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,12 +1,40 @@
 
+# module Shout
+
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+
+#   def self.yelling_happily(words)
+#     words + "!!!" + " :D"
+#   end
+
+# end
+
 module Shout
 
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
+  def yell_angrily(words)
+    puts words + "!!!" + " :("
   end
 
-  def self.yelling_happily(words)
-    words + "!!!" + " :D"
+  def yelling_happily(words)
+    puts words + "!!!" + " :D"
   end
 
 end
+
+class Baby
+  include Shout
+end
+
+class Referee
+  include Shout
+end
+
+#Driver Code
+
+baby = Baby.new
+referee = Referee.new
+
+baby.yell_angrily("badabaubu")
+referee.yell_angrily("Stop")
