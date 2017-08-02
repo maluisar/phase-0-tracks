9a082cbbd17f4567d0e1d0f3be3f02475ee3ddbb
Maria Luisa Rgit config --global user.name Eleanor <maluisa.rc@gmail.com>
2017-08-01 23:29:36 -0700

    Add a new class to puppy methods file

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -26,7 +26,6 @@ end
 class Puppy
   def dog_activity
     selector = 1 + rand(3)
-    p selector
     if selector == 1
       puts "Puppy is sleeping/resting"
     elsif selector == 2
@@ -37,9 +36,56 @@ class Puppy
   end
 end
 
+class Puppy
+  def initialize
+    puts "Initialize new puppy instance..."
+  end
+end
+
 scout = Puppy.new
 
 scout.fetch("ball")
 scout.speak(4)
 scout.roll_over
 scout.dog_activity
+
+class School
+  def initialize
+    puts "Inititating School"
+  end
+end
+
+class School
+  def attendance
+    puts "Student attended class"
+  end
+end
+
+class School
+  def teacher_advice(grade)
+    if grade == "A" || grade =="A+" || grade=="B+"
+      puts "Keep doing what you are doing"
+    elsif grade == "B" || grade == "C+" || grade == "C"
+      puts "You have to put a little more effort"
+    else
+      puts "You have to put a lot more effort and come to office hours"
+    end
+  end
+end
+
+student_count = 1
+instances_list = []
+
+until student_count == 51
+  instance = School.new
+  instances_list << instance
+  student_count +=1
+end
+
+instances_list.each do |instance|
+  instance.attendance
+end
+
+instances_list.each do |instance|
+  instance.teacher_advice("A+")
+end
