8ed7f1ab508795546c67bd7ee4e58d790079b8cf
Maria Luisa Rgit config --global user.name Eleanor <maluisa.rc@gmail.com>
2017-08-01 20:30:10 -0700

    Add methods to Puppy Class

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,45 @@
 class Puppy
-
   def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
   end
-
 end
 
+class Puppy
+  def speak(i)
+    puts "Woof! "*i.to_i
+  end
+end
+
+class Puppy
+  def roll_over
+    puts "Rolling over!"
+  end
+end
+
+class Puppy
+  def dog_years (i)
+    puts i*7
+  end
+end
+
+class Puppy
+  def dog_activity
+    selector = 1 + rand(3)
+    p selector
+    if selector == 1
+      puts "Puppy is sleeping/resting"
+    elsif selector == 2
+      puts "Puppy is playing"
+    else
+      puts "Puppy is eating"
+    end
+  end
+end
+
+scout = Puppy.new
+
+scout.fetch("ball")
+scout.speak(4)
+scout.roll_over
+scout.dog_activity
