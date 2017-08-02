b0b86a98db21d1934443b54a4bf8ca422109758c
Maria Luisa Rgit config --global user.name Eleanor <maluisa.rc@gmail.com>
2017-08-02 15:14:41 -0700

    Write pseudocode for word guesser game

---
+++ ruby/game/game.rb
@@ -0,0 +1,36 @@
+
+#Create necessary methods:
+
+#method receive the input word from user1
+#input: word
+#steps: take word/string and split it into an array
+#output: array
+
+#method to check if the letter input from user2 is in the word
+#input: letter
+#steps: check if letter is in array, return true if it is.
+#output: true/false
+
+#method to subsitute "_" by right letter
+#input: index, letter
+#steps: substitue letter at right index
+#output: updated array
+
+#Print updated "______" with guessed letters
+#input:
+#steps: print game progress using #each
+#output: updated array printed
+
+#Add driver code
+# => Ask user for word and store in a variable
+# => Call method to store word
+# => Create array of empty "_" with the length of the word
+# => Set variable for guesses limit with the length of the word * 2
+# => Create a condition for repeated letters
+# => Ask user to start guessing letters
+# => Call method to check input letter
+# => Update array to print and print it
+# => Ask for the next letter
+# => Add win/lose message
+
+
