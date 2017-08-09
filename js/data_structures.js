282e91f42338ceea98ccf854cffea446d8745fb3
Maria Luisa Rgit config --global user.name Eleanor <maluisa.rc@gmail.com>
2017-08-09 11:49:19 -0700

    Create Car constructor function

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,3 +1,5 @@
+//Horses objects
+
 var colors = ["raspberry ombre","purple", "metallic yellow", "sky blue"];
 var names = ["Lola", "Horace", "Gracie", "Seabiscuit"];
 
@@ -10,9 +12,26 @@ for (var i=0; i < colors.length; i++){
   horses[names[i]] = colors[i];
 }
 
+//Car constructor
+
+function Car(color, year, maxSpeed) {
+
+  this.color = color;
+  this.year = year;
+  this.maxSpeed = maxSpeed;
+  this.boostMaxSpeed = function(){
+    this.maxSpeed += 100;
+    console.log("Yours max-speed is now " + maxSpeed);
+  }
+}
 
 //Driver Code
 
 console.log(colors);
 console.log(names);
 console.log(horses);
+
+var mcQueen = new Car("red", 2017, 200);
+console.log(mcQueen);
+mcQueen.boostMaxSpeed();
+console.log(mcQueen);
